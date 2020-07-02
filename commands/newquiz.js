53ad7e46f4c1b121712018dfc26706b89690ef00
ambravl <ambra.loos@gmail.com>
2020-07-02 06:04:20 -0300

    due to shortcomings in my personality the emoji in a message must be separated by spaces

--- commands/newquiz.js
+++ commands/newquiz.js
@@ -6,6 +6,7 @@ module.exports = {
   async run(client, message, args) {
     const emojiRegex = require('emoji-regex')();
     const emoji = message.content.match(emojiRegex);
+    emoji.concat(message.content.match(/(?<=<:\w+:)\d+(?=>)/g));
     const rightAnswer = args[0];
     const content = message.content.replace(`${client.cfg.prefix}newquiz ${rightAnswer}`, '').trim();
     client.channels.cache.get(client.cfg.quizChannel).send(content).then((msg) => {
