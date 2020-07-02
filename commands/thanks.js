a0a6cd0a643b7ab339b39778b0ff1e51f5e1fb72
ambravl <ambra.loos@gmail.com>
2020-07-02 09:39:31 -0300

    added thanks command

--- commands/thanks.js
+++ commands/thanks.js
@@ -3,12 +3,22 @@ module.exports = {
   description: 'Thanks an user so they can unlock more channels',
   cooldown: 5,
   permLevel: 0,
+  aliases: ['thank', 'ty'],
   async run(client, message) {
-    const thanked = message.mentions.first();
-    client.db.zadd(`thanks:${thanked.id}`, 1, message.author.id, function (err, res) {
-      if(err) return client.catch(err);
-      if(res === 0) return client.error('You already thanked this person!', message);
+    const thanked = message.mentions.members.first();
+    client.db.zscore(`thanks:${thanked.id}`, message.author.id, function (err, res) {
+      if (err) return client.catch(err, message);
+      if (res && res !== '(nil)' && message.createdTimestamp - parseInt(res) < client.cfg.thankLimit)
+        return client.error('You already thanked this person!', message);
+      client.db.zadd(`thanks:${thanked.id}`, message.createdTimestamp, message.author.id);
+      client.db.zincrby(`thanks:${thanked.id}`, 1, 'points', (err, res) => {
+        if(err) return client.catch(err, message);
+        const i = client.cfg.starUp.indexOf(parseInt(res));
+        if(i === -1) return message.channel.send(`Successfully thanked ${thanked}!`);
+        thanked.roles.add(message.guild.roles.cache.find((role) => role.name === `${i} star`));
+        if(i !== 1) thanked.roles.remove(message.guild.roles.cache.find((role) => role.name === `${i-1} star`));
+        message.channel.send(`Congratulations, ${thanked}! You just leveled up to ${i} stars!`)
+      });
     })
-
-  },
+  }
 };
