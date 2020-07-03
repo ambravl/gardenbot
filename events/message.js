8192d596a7836cccd89ec60c892637945f34172b
ambravl <ambra.loos@gmail.com>
2020-07-03 18:36:48 -0300

    every command now gets a message's content without the command part

--- events/message.js
+++ events/message.js
@@ -7,19 +7,23 @@ module.exports = async (client, message) => {
   const command = client.commands.get(commandName)
     || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-  if (!(command && client.hasPermission(message, command))) return;
+  console.log(`${message.author.username} tried running ${command.name} in ${message.channel.name || 'DMs'}!`);
+
+  if (!(command && client.hasPermission(message, command))) return client.error('No commands found with that name!');
+
+  message.content = message.content.slice(client.cfg.prefix.length + commandName.length).trim();
+  message.commmand = commandName;
 
   if (command.guildOnly && !message.guild) {
     return client.error('I can\'t execute that command inside DMs!', message);
   }
 
-  if (command.args && args.length < 1) {
-    return client.error(
-      `You didn't provide any arguments, ${message.author}!` +
-      command.usage ? `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\`` : '',
-      message
-    )
-  }
+  if(client.wrongArgumentNumber(command.args, args.length))
+      return client.error(
+        `Wrong number of arguments, ${message.author}!` +
+        command.usage ? `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\`` : '',
+        message
+      );
 
   try {
     command.run(client, message, args);
