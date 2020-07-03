0fcfb8f90892e9f25960df39c4300b72bd8920da
ambravl <ambra.loos@gmail.com>
2020-07-03 18:35:18 -0300

    added support for embeds and attachments

--- commands/echo.js
+++ commands/echo.js
@@ -4,14 +4,29 @@ module.exports = {
   usage: '<#channel (optional)> <message>',
   cooldown: 5,
   permLevel: 1,
+  args: [1],
   async run(client, message, args) {
-    if(args[0].startsWith('<#')){
-      message.mentions.channels.first().send(message.content.slice(client.cfg.prefix.length + 6 + args[0].length))
-        .catch((err) => {client.catch(err)});
+    let channel = message.channel;
+    let send = {};
+    if(args[0].startsWith('<#')) {
+      message.content = message.content.slice(args[0].length).trim();
+      channel = message.mentions.channels.first();
     }
-    else{
-      message.channel.send(message.content.slice(client.cfg.prefix.length + 5))
+    if(message.content.startsWith('{')){
+      try{
+        const obj = JSON.parse(JSON.stringify(message.content));
+        send.embed = JSON.parse(obj).embed;
+        console.log(send.embed);
+        const content = JSON.parse(obj).content;
+        if(content) send.content = content;
+      } catch(err) {
+        console.error(err);
+        send.content = message.content;
+      }
+    } else send.content = message.content;
+    if(message.attachments.first()) send.files = message.attachments;
+    send.split = true;
+      channel.send(send)
         .catch((err) => {client.catch(err)});
-    }
   },
 };
